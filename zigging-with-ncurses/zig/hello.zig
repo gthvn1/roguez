63c8636d3238a2ab6f4e6d0aaa03a5cf17508cfc
Guillaume <guillaume.thouvenin@vates.tech>
2025-10-20 13:39:07 +0200

    [ncurses] print the four corners

--- zigging-with-ncurses/zig/hello.zig
+++ zigging-with-ncurses/zig/hello.zig
@@ -18,14 +18,26 @@ pub fn main() void {
     _ = n.start_color();
 
     _ = n.init_pair(1, n.COLOR_GREEN, n.COLOR_BLACK);
-    _ = n.attron(n.COLOR_PAIR(1));
+    _ = n.init_pair(2, n.COLOR_BLACK, n.COLOR_GREEN);
 
-    const max_x = n.getmaxx(stdscr);
-    const max_y = n.getmaxy(stdscr);
+    const max_x = n.getmaxx(stdscr) - 1;
+    const max_y = n.getmaxy(stdscr) - 1;
 
     // As we are printing value calling C function we don't need to do any cast.
+    _ = n.attron(n.COLOR_PAIR(1));
     _ = n.mvprintw(1, 1, "Screen size: %d x %d\n", max_y, max_x);
-    _ = n.refresh(); // Print it on the real screen
 
+    // Let's print the four corners
+    // A: Upper left is Y:0 X:0
+    // B: Upper right is Y:0 X:max_x
+    // C: Bottom left is Y:max_y X:0
+    // D: Bottom right is Y:max_y X:max_x
+    _ = n.attron(n.COLOR_PAIR(2));
+    _ = n.mvprintw(0, 0, "A");
+    _ = n.mvprintw(0, max_x, "B");
+    _ = n.mvprintw(max_y, 0, "C");
+    _ = n.mvprintw(max_y, max_x, "D");
+
+    _ = n.refresh(); // Print it on the real screen
     _ = n.getch(); // Wait for user input
 }
