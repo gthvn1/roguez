6cdeb205392f98aeba0c08e94b4d1025aa01ebad
Guillaume <guillaume.thouvenin@vates.tech>
2025-10-20 10:57:11 +0200

    Print the size of terminal

--- zigging-with-ncurses/zig/hello.zig
+++ zigging-with-ncurses/zig/hello.zig
@@ -1,12 +1,21 @@
 const std = @import("std");
-const ncurses = @cImport({
+const n = @cImport({
     @cInclude("ncurses.h");
 });
 
+// https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/
 pub fn main() void {
-    _ = ncurses.initscr(); // Start curses mode
-    _ = ncurses.printw("Hello World"); // Print Hello World
-    _ = ncurses.refresh(); // Print it on the real screen
-    _ = ncurses.getch(); // Wait for user input
-    _ = ncurses.endwin(); // End curses mode
+    const stdscr = n.initscr();
+    // In case of errors a message is written to stderr and initscr
+    // exists.
+
+    const max_x = n.getmaxx(stdscr);
+    const max_y = n.getmaxy(stdscr);
+
+    // As we are printing value calling C function we don't need to do any cast.
+    _ = n.mvprintw(1, 1, "Screen size: %d x %d\n", max_y, max_x);
+    _ = n.refresh(); // Print it on the real screen
+
+    _ = n.getch(); // Wait for user input
+    _ = n.endwin(); // End curses mode
 }
