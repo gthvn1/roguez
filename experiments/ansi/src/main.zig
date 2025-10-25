bba63dbd393db12975adb8f681023342b319319f
Guillaume <gthvn1@gmail.com>
2025-10-25 23:41:35 +0200

    [ansi] use map string

--- experiments/ansi/src/main.zig
+++ experiments/ansi/src/main.zig
@@ -1,11 +1,102 @@
 const std = @import("std");
 
+pub const map_str: []const u8 =
+    \\########################################
+    \\#.......#........................&.....#
+    \\#..a....#..........................&...#
+    \\#.......#........&..............@......#
+    \\#.......E.........................#....#
+    \\####A####.........................#....#
+    \\#.......#.......&......................#
+    \\#.......#......#.#....b..........f.....#
+    \\#...$...#......#.#........c............#
+    \\#.......#......#e#............d........#
+    \\#########......###.....................#
+    \\#......................................#
+    \\#......................................#
+    \\########################################
+;
+
+const Pos = struct {
+    line: usize,
+    column: usize,
+};
+
+const MapError = error{
+    Empty,
+    RobotNotFound,
+};
+
+const Map = struct {
+    robot: Pos,
+    map: [][]const u8,
+    allocator: *std.mem.Allocator,
+
+    pub fn of_string(allocator: *std.mem.Allocator, str: []const u8) !Map {
+        var robot: ?Pos = null;
+
+        var line_iter = std.mem.splitAny(u8, str, "\n");
+
+        // We need to know the numbers of lines and the number of columns.
+        // So we do a first iteration to compute this. In the same time we will
+        // try to find the robot.
+        var lines: usize = 0;
+        while (line_iter.next()) |l| {
+            // Check if the robot is on this line
+            if (std.mem.indexOfScalar(u8, l, '@')) |col| {
+                robot = .{
+                    .line = @intCast(lines),
+                    .column = @intCast(col),
+                };
+            }
+            lines += 1;
+        }
+
+        if (lines == 0) {
+            return MapError.Empty;
+        }
+
+        if (robot) |r| {
+            var map = try allocator.alloc([]const u8, lines);
+
+            // Now we can do the second iteration
+            line_iter = std.mem.splitAny(u8, str, "\n");
+            var current_line: usize = 0;
+            while (line_iter.next()) |s| {
+                const l = try allocator.alloc(u8, s.len);
+                std.mem.copyForwards(u8, l, s);
+                map[current_line] = l;
+                current_line += 1;
+            }
+
+            return .{
+                .map = map,
+                .robot = r,
+                .allocator = allocator,
+            };
+        } else {
+            return MapError.RobotNotFound;
+        }
+    }
+
+    pub fn free(self: *const Map) void {
+        for (self.map) |row| {
+            self.allocator.free(row);
+        }
+        self.allocator.free(self.map);
+    }
+};
+
 pub fn main() !void {
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-
     const stdout = &stdout_writer.interface;
 
+    const GpaType = std.heap.GeneralPurposeAllocator(.{});
+    var gpa = GpaType{};
+    var allocator = gpa.allocator();
+
+    const m = try Map.of_string(&allocator, map_str);
     // https//gist.github.com/ConnerWill/d4b6c776b509add763e17f9f113fd25b
     // ESC     -> \x1b
     // Unicode -> \u001b
@@ -18,21 +109,28 @@ pub fn main() !void {
     // Print a red '@' for the player
     try stdout.print("\x1b[31m{s}\x1b[0m", .{"Hello, Sailor!"});
 
-    // Move cursor to (2,3) and print a green '@'
-    try stdout.print("\x1b[2;3H\x1b[32m@\x1b[0m", .{});
-
     // moves cursor to beginning of next line
     try stdout.print("\x1b[#E", .{});
 
-    const line = [_]u8{ '#', '#', '#', '#' };
-
     // Print a yellow line
-    try stdout.print("\x1b[33m", .{});
-    for (line) |c| {
-        try stdout.print("{c}", .{c});
+    for (m.map) |line| {
+        for (line) |c| {
+            switch (c) {
+                '@' => {
+                    try stdout.print("\x1b[32m", .{});
+                    try stdout.print("{c}", .{c});
+                    try stdout.print("\x1b[0m", .{});
+                },
+                else => {
+                    try stdout.print("\x1b[33m", .{});
+                    try stdout.print("{c}", .{c});
+                    try stdout.print("\x1b[0m", .{});
+                },
+            }
+        }
+        try stdout.print("\n", .{});
     }
     try stdout.print("\n", .{});
-    try stdout.print("\x1b[0m", .{});
 
     // Don't forget to flush
     try stdout.flush();
