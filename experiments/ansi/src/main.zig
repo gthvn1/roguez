7e016b21be305f0f1b051d66714f4402bfb37d6f
Guillaume <guillaume.thouvenin@vates.tech>
2025-10-26 17:12:28 +0100

    [ansi] split functions

--- experiments/ansi/src/main.zig
+++ experiments/ansi/src/main.zig
@@ -30,6 +30,8 @@ const MapError = error{
 const Map = struct {
     robot: Pos,
     map: [][]const u8,
+    width: usize, // The number of columns starting from 0
+    height: usize, // The number of lines starting from 0
     allocator: *std.mem.Allocator,
 
     pub fn of_string(allocator: *std.mem.Allocator, str: []const u8) !Map {
@@ -40,24 +42,29 @@ const Map = struct {
         // We need to know the numbers of lines and the number of columns.
         // So we do a first iteration to compute this. In the same time we will
         // try to find the robot.
-        var lines: usize = 0;
-        while (line_iter.next()) |l| {
+        var height: usize = 0;
+        var width: usize = 0;
+
+        while (line_iter.next()) |line| {
             // Check if the robot is on this line
-            if (std.mem.indexOfScalar(u8, l, '@')) |col| {
+            if (std.mem.indexOfScalar(u8, line, '@')) |col| {
                 robot = .{
-                    .line = @intCast(lines),
+                    .line = @intCast(height),
                     .column = @intCast(col),
                 };
             }
-            lines += 1;
+            // Always update width it is not that important.
+            // All lines should have the same len.
+            width = line.len;
+            height += 1;
         }
 
-        if (lines == 0) {
+        if (height == 0) {
             return MapError.Empty;
         }
 
         if (robot) |r| {
-            var map = try allocator.alloc([]const u8, lines);
+            var map = try allocator.alloc([]const u8, height);
 
             // Now we can do the second iteration
             line_iter = std.mem.splitAny(u8, str, "\n");
@@ -71,6 +78,8 @@ const Map = struct {
 
             return .{
                 .map = map,
+                .height = height,
+                .width = height,
                 .robot = r,
                 .allocator = allocator,
             };
@@ -97,74 +106,130 @@ pub fn main() !void {
     var allocator = gpa.allocator();
 
     const m = try Map.of_string(&allocator, map_str);
+
+    // We need to set the terminal in Raw mode to avoid pressing enter
+    // TODO:
+    //   - Should we restore the old_settings?
+    set_raw_mode();
+
+    try erase_screen(stdout);
+
+    // - 1st line is the title
+    try set_title(stdout);
+
+    // - then we have the maze
+    // Set cursor position right after the maze when writing the result
+    // of read_char. It will erase the caracter we pressed.
+    try draw_maze(stdout, &m);
+
+    if (read_char()) |carlu| {
+        try stdout.print("\x1b[{d};1H", .{1 + m.height});
+        try stdout.print("\nYou pressed: 0x{x}\n", .{carlu});
+    } else {
+        try stdout.print("\x1b[{d};1H", .{1 + m.height + 1});
+        try stdout.print("\nFailed to read a char\n", .{});
+    }
+    try stdout.flush();
+}
+
+fn set_raw_mode() void {
+    var settings: std.os.linux.termios = undefined;
+    _ = std.os.linux.tcgetattr(0, &settings);
+
+    // Disabling canonical mode allow the input to be immediatly available
+    settings.lflag.ICANON = false;
+
+    _ = std.os.linux.tcsetattr(0, std.posix.TCSA.NOW, &settings);
+}
+
+fn erase_screen(out: *std.Io.Writer) !void {
+    try out.writeAll("\x1b[2J");
+
+    // Reset all modes
+    try out.print("\x1b[0m", .{});
+}
+
+fn set_title(out: *std.Io.Writer) !void {
+    // Move cursor to (1,10)
+    try out.print("\x1b[1;12H", .{});
+
+    // Print a red , bold, underline, italic title
+    try out.print("\x1b[1;3;4;31m{s}\x1b[0m", .{"Welcome to RogueZ !"});
+
+    // Reset all modes
+    try out.print("\x1b[0m", .{});
+}
+
+fn draw_maze(out: *std.Io.Writer, map: *const Map) !void {
     // https://gist.github.com/ConnerWill/d4b6c776b509add763e17f9f113fd25b
     // ESC     -> \x1b
     // Unicode -> \u001b
 
-    try stdout.writeAll("\x1b[2J");
-
-    // Move cursor to (1,10)
-    try stdout.print("\x1b[1;12H", .{});
-
-    // Print a red , bold, underline, italic title
-    try stdout.print("\x1b[1;3;4;31m{s}\x1b[0m", .{"Welcome to RogueZ !"});
-
-    // moves cursor to beginning of next line
-    try stdout.print("\x1b[#E", .{});
+    // moves cursor to beginning of line 2
+    try out.print("\x1b[2;1H", .{});
 
     // Print a yellow line
-    for (m.map) |line| {
+    for (map.map) |line| {
         for (line) |c| {
             switch (c) {
                 '&' => {
                     // Yellow
-                    try stdout.print("\x1b[1;33m", .{});
-                    try stdout.print("{c}", .{c});
+                    try out.print("\x1b[1;33m", .{});
+                    try out.print("{c}", .{c});
                 },
                 '$' => {
                     // Red
-                    try stdout.print("\x1b[1;31m", .{});
-                    try stdout.print("{c}", .{c});
+                    try out.print("\x1b[1;31m", .{});
+                    try out.print("{c}", .{c});
                 },
                 '@' => {
                     // Green
-                    try stdout.print("\x1b[1;32m", .{});
-                    try stdout.print("{c}", .{c});
+                    try out.print("\x1b[1;32m", .{});
+                    try out.print("{c}", .{c});
                 },
                 'a'...'z' => {
                     // Magenta
-                    try stdout.print("\x1b[1;35m", .{});
-                    try stdout.print("{c}", .{c});
+                    try out.print("\x1b[1;35m", .{});
+                    try out.print("{c}", .{c});
                 },
                 'A'...'Z' => {
                     // Blue
-                    try stdout.print("\x1b[1;34m", .{});
-                    try stdout.print("{c}", .{c});
+                    try out.print("\x1b[1;34m", .{});
+                    try out.print("{c}", .{c});
                 },
                 '.' => {
                     // Black
-                    try stdout.print("\x1b[1;30m", .{});
-                    try stdout.print(" ", .{});
+                    try out.print("\x1b[1;30m", .{});
+                    try out.print(" ", .{});
                 },
                 '#' => {
                     // Cyan and don't use bold
-                    try stdout.print("\x1b[0;36m", .{});
-                    try stdout.print("{c}", .{c});
+                    try out.print("\x1b[0;36m", .{});
+                    try out.print("{c}", .{c});
                 },
                 else => {
                     // Default
-                    try stdout.print("\x1b[0;39m", .{});
-                    try stdout.print("{c}", .{c});
+                    try out.print("\x1b[0;39m", .{});
+                    try out.print("{c}", .{c});
                 },
             }
 
             // Reset all modes
-            try stdout.print("\x1b[0m", .{});
+            try out.print("\x1b[0m", .{});
         }
-        try stdout.print("\n", .{});
+        try out.print("\n", .{});
     }
-    try stdout.print("\n", .{});
+    try out.print("\n", .{});
 
     // Don't forget to flush
-    try stdout.flush();
+    try out.flush();
+}
+
+fn read_char() ?u8 {
+    var stdin_buffer: [1]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
+    const stdin = &stdin_reader.interface;
+
+    const carlu: u8 = stdin.peekByte() catch return null;
+    return carlu;
 }
